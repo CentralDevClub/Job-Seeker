3784aa59d9254e694fea97e607fd36ad455c8fd9
Philip Purwoko <Philippurwoko123@gmail.com>
2020-07-20 12:27:59 +0700

    Menambahkan struktur backend

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+	res.render('home');
+});
+
+const port = 8000;
+app.listen(port);
+console.log(`Website Successfully Deployed! Go to : http//localhost:${port}`);
